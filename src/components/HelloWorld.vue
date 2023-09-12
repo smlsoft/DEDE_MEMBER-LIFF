40efb052dc460b80b976b42b9443aea48439ab80
NontScramber48 <103891946+NontScramber48@users.noreply.github.com>
2023-09-12 14:20:30 +0700

    Update Page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,132 @@
 <template>
-  <div></div>
+  <section
+    class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8"
+  >
+    <div
+      class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"
+    />
+    <div
+      class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"
+    />
+    <div class="mx-auto max-w-2xl lg:max-w-4xl">
+      <img />
+      <img
+        class="mx-auto h-10 w-10 rounded-full"
+        :src="pictureUrl"
+        alt="User's picture"
+        v-if="pictureUrl"
+      />
+      <figure class="mt-10">
+        <blockquote
+          class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9"
+        >
+          <p>“Hi {{ displayName }}”</p>
+        </blockquote>
+        <figcaption class="mt-10">
+          <div
+            class="mt-4 flex items-center justify-center space-x-3 text-base"
+          >
+            <div class="font-semibold text-gray-900">User ID</div>
+            <svg
+              viewBox="0 0 2 2"
+              width="3"
+              height="3"
+              aria-hidden="true"
+              class="fill-gray-900"
+            >
+              <circle cx="1" cy="1" r="1" />
+            </svg>
+            <div class="text-gray-600">{{ userId }}</div>
+          </div>
+          <div
+            class="mt-4 flex items-center justify-center space-x-3 text-base"
+          >
+            Status Message: {{ statusMessage }}
+            <p>OS: {{ os }}</p>
+            <p>Language: {{ language }}</p>
+            <p>Version: {{ version }}</p>
+            <p>Access Token: {{ accessToken }}</p>
+            <p>Is In Client: {{ isInClient }}</p>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  </section>
   <div>
-    <img :src="pictureUrl" alt="User's picture" v-if="pictureUrl" />
+    <!-- Profile Card -->
 
-    <!-- <h1>About Us</h1> -->
-    <p>Hi {{ displayName }}</p>
+    <!-- ... (Rest of your existing template) -->
+  </div>
+  <div>
+    <p-card title="Profile Details" :style="{ width: '300px', margin: 'auto' }">
+      <template #title>
+        <img
+          :src="pictureUrl"
+          alt="User's picture"
+          v-if="pictureUrl"
+          class="p-mb-2"
+        />
+        <div class="p-text-center">{{ displayName }}</div>
+      </template>
+      <template #content>
+        <p>Display Name: {{ displayName }}</p>
+
+        <p></p>
+        <p>Status Message: {{ statusMessage }}</p>
+      </template>
+    </p-card>
+    <img :src="pictureUrl" alt="User's picture" v-if="pictureUrl" />
+    <InputNumber v-model="value1" inputId="integeronly" />
+
+    <p-button label="Click Me"></p-button>
+
+    <!-- Display user details -->
+    <!-- <p></p>
     <p>Display Name: {{ displayName }}</p>
     <p>User ID: {{ userId }}</p>
-    <p>Status Message: {{ statusMessage }}</p>
-    <!-- 
+    <p></p> -->
+  </div>
+  <!-- <div class="surface-ground text-center px-0 sm:px-4 py-8 md:px-6 lg:px-8">
+    <Carousel :value="company">
+      <template #item="slotProps">
+        <div class="flex justify-content-center">
+          <div
+            class="surface-card shadow-2 px-2 md:px-6 py-5 border-round mb-4 text-900 text-2xl line-highlight-3"
+            style="max-width: 600px"
+          >
+            {{ slotProps.data.text }}
+          </div>
+        </div>
+
+        <img
+          :src="
+            'images/blocks/testimonial/testimonials-' +
+            slotProps.data.image +
+            '.png'
+          "
+          width="56"
+          height="56"
+          :alt="slotProps.data.name"
+          class="mb-3"
+        />
+        <div class="text-900 font-bold line-height-3">
+          {{ slotProps.data.name }}
+        </div>
+        <div class="text-600 line-height-3 mb-4">Company Title</div>
+      </template>
+    </Carousel>
+  </div> -->
+  <!-- 
     <p>OS: {{ os }}</p>
     <p>Language: {{ language }}</p>
     <p>Version: {{ version }}</p>
     <p>Access Token: {{ accessToken }}</p>
     <p>Is In Client: {{ isInClient }}</p> -->
-    <!-- 
+  <!-- 
     <button id="btnLogout" v-if="!isInClient">Logout</button>
     <button id="btnMsg" v-if="isInClient">Message</button>
     <button id="btnScanCode" v-if="isInClient">Scan Code</button>
     <button id="BtnClose" v-if="isInClient">Close</button> -->
-  </div>
   <!-- <div v-if="profile">
     <p>Display Name: {{ displayName }}</p>
     <p>
@@ -32,8 +139,17 @@
 </template>
 
 <script setup>
-import liff from "@line/liff";
 import { ref, onMounted } from "vue";
+import liff from "@line/liff";
+import "primevue/resources/themes/saga-blue/theme.css"; // theme
+import "primevue/resources/primevue.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
+const pButton = Button;
+
+// Import PrimeVue button component
+import Button from "primevue/button";
+
+// Define the button component
 
 const os = ref("");
 const language = ref("");
@@ -49,8 +165,8 @@ const displayName = ref("");
 onMounted(async () => {
   try {
     await liff.init({ liffId: "2000714922-XOb4DG4e" });
-    getEnvironment();
     getUserprofile();
+    getEnvironment();
   } catch (error) {
     console.error("Error initializing LIFF:", error);
   }
@@ -80,3 +196,7 @@ function getEnvironment() {
   isInClient.value = liff.isInClient();
 }
 </script>
+
+<style>
+/* Add any style you want here */
+</style>
