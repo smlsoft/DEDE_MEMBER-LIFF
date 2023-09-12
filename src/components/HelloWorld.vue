c4ae3e8ffe9c26e108f197225965a718bc70862b
NontScramber48 <103891946+NontScramber48@users.noreply.github.com>
2023-09-12 09:16:29 +0700

    Update Page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,11 +5,20 @@
     <p>Version: {{ version }}</p>
     <p>Access Token: {{ accessToken }}</p>
     <p>Is In Client: {{ isInClient }}</p>
-
+    <!-- 
     <button id="btnLogout" v-if="!isInClient">Logout</button>
     <button id="btnMsg" v-if="isInClient">Message</button>
     <button id="btnScanCode" v-if="isInClient">Scan Code</button>
-    <button id="BtnClose" v-if="isInClient">Close</button>
+    <button id="BtnClose" v-if="isInClient">Close</button> -->
+  </div>
+  <div v-if="profile">
+    <p>Display Name: {{ displayName }}</p>
+    <p>
+      Picture URL:
+      <img :src="pictureUrl" alt="User's picture" v-if="pictureUrl" />
+    </p>
+    <p>User ID: {{ userId }}</p>
+    <p>Status Message: {{ statusMessage }}</p>
   </div>
 </template>
 
@@ -22,15 +31,37 @@ const language = ref("");
 const version = ref("");
 const accessToken = ref("");
 const isInClient = ref(false);
+const profile = ref("");
+const pictureUrl = ref("");
+const userId = ref("");
+const statusMessage = ref("");
+const displayName = ref("");
 
 onMounted(async () => {
   try {
     await liff.init({ liffId: "2000714922-XOb4DG4e" });
     getEnvironment();
+    getUserprofile();
   } catch (error) {
     console.error("Error initializing LIFF:", error);
   }
 });
+
+async function getUserprofile() {
+  try {
+    profile.value = await liff.getProfile();
+    console.log(profile.value);
+
+    if (profile.value) {
+      pictureUrl.value = profile.value.pictureUrl;
+      userId.value = profile.value.userId;
+      statusMessage.value = profile.value.statusMessage;
+      displayName.value = profile.value.displayName;
+    }
+  } catch (error) {
+    console.error("Error getting user profile:", error);
+  }
+}
 
 function getEnvironment() {
   os.value = liff.getOS();
